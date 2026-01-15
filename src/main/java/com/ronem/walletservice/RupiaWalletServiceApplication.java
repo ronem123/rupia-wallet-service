64e1ac04111689dec37a5d7f6115a220e424061d
Ram Mandal <ram.mandal@8squarei.com>
2026-01-15 11:59:17 +0545

    initial commit

---
+++ src/main/java/com/ronem/walletservice/RupiaWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ronem.walletservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RupiaWalletServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RupiaWalletServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ronem/walletservice/RupiaWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronem.walletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RupiaWalletServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
