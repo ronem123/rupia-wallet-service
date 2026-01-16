c2012bb2217393ded179705b7779d4d4728cb104
Ram Mandal <ram.mandal@8squarei.com>
2026-01-16 09:58:21 +0545

    configure eureka discovery client

--- src/main/java/com/ronem/walletservice/RupiaWalletServiceApplication.java
+++ src/main/java/com/ronem/walletservice/RupiaWalletServiceApplication.java
@@ -2,8 +2,10 @@ package com.ronem.walletservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RupiaWalletServiceApplication {
 
     public static void main(String[] args) {
